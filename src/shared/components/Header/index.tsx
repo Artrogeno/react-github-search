0ec3d0e220c1d7b4af3ca5ee91468a2718ad609d
Arthur Costa da Silva <arthur.silva@pampa.compasso>
2020-07-16 10:51:05 -0300

    feat: create header on shared component

---
+++ src/shared/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container, Title } from './styles'
+
+const Header = () => {
+  return (
+    <Container>
+      <Title>Github search</Title>
+    </Container>
+  )
+}
+
+export default Header
---
+++ src/shared/components/Header/styles.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  width: 80vw;
+  height: var(--header-h);
+`
+
+export const Title = styled.h1`
+  font-weight: 400;
+`
